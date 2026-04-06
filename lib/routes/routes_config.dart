0501d2372e2b547947635ec52979a92ef5ae7231
Vinícius Rodrigues <156464836+VIN1RM@users.noreply.github.com>
2026-04-06 17:33:13 -0300

    fix(review-order): remove search bar and center payment method chips

--- lib/routes/routes_config.dart
+++ lib/routes/routes_config.dart
@@ -7,15 +7,17 @@ import 'package:meatshop_mobile/ui/screens/auth/register_screen.dart';
 import 'package:meatshop_mobile/ui/screens/auth/change_password.dart';
 import 'package:meatshop_mobile/ui/screens/auth/select_register_screen.dart';
 import 'package:meatshop_mobile/ui/widgets/app_shell.dart';
+import 'package:meatshop_mobile/ui/screens/cart/review_order_screen.dart';
 
 Map<String, WidgetBuilder> buildRoutes() {
   return {
-    AppRoutes.splash:          (_) => const SplashPage(),
-    AppRoutes.welcome:         (_) => const WelcomePage(),
-    AppRoutes.login:           (_) => const LoginPage(),
-    AppRoutes.selectRegister:  (_) => const SelectRegisterPage(),
-    AppRoutes.register:        (_) => const RegisterPage(),
+    AppRoutes.splash:         (_) => const SplashPage(),
+    AppRoutes.welcome:        (_) => const WelcomePage(),
+    AppRoutes.login:          (_) => const LoginPage(),
+    AppRoutes.selectRegister: (_) => const SelectRegisterPage(),
+    AppRoutes.register:       (_) => const RegisterPage(),
     AppRoutes.changePassword:  (_) => const ChangePasswordPage(),
-    AppRoutes.shell:           (_) => const AppShell(),
+    AppRoutes.shell:          (_) => const AppShell(),
+    AppRoutes.reviewOrder:    (_) => const ReviewOrderScreen(),
   };
 }
